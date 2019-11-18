0a59c290df9eac20c74ef15f403d7529ab004786
Justin Capalbo <capalbo.justin@gmail.com>
2019-11-18 10:57:03 -0500

    First pass at a coal heuristic

--- custom-code.js
+++ custom-code.js
@@ -1,9 +1,9 @@
 /* Zebra stuff */
 
 const getAverageTitaniumPerTrade = () => {
-    let standingRatio = this.game.getEffect("standingRatio");
+    let standingRatio = game.getEffect("standingRatio");
 
-    if (this.game.prestige.getPerk("diplomacy").researched) {
+    if (game.prestige.getPerk("diplomacy").researched) {
         standingRatio += 10;
     }
 
@@ -12,7 +12,7 @@ const getAverageTitaniumPerTrade = () => {
     const tradeFailProbability = zebras.attitude === "hostile" ? (1 - zebras.standing - standingRatio / 100) : 0;
     const tradeSuccessProbability = 1 - tradeFailProbability;
 
-    const shipAmount = this.game.resPool.get("ship").value;
+    const shipAmount = game.resPool.get("ship").value;
     const titaniumProbability = 0.15 + shipAmount * 0.0035;
     const titaniumRatio = 1 + (shipAmount / 100) * 2; // 2% more titanium per ship
 
@@ -28,35 +28,107 @@ const getOptimalZebraTrades = () => {
     return Math.floor(titaniumBelowMax / avgTitaniumPerTrade) + 1;
 }
 
-const tradeToCapTitanium = () => {
-    const gold = game.resPool.get("gold").value;
-    const slabs = game.resPool.get("slab").value;
-    const catpower = game.resPool.get("manpower").value;
+const getCappedResourcesForTrading = (tradeResName, tradeResAmt) => {
     const tradeCaps = [
-        { res: "gold", amt: Math.floor(gold/15) },
-        { res: "slab", amt: Math.floor(slabs/50) },
-        { res: "catpower", amt: Math.floor(catpower/50) },
-    ];
+        { res: "gold", cost: 15 },
+        { res: "manpower", cost: 50 },
+        { res: tradeResName, cost: tradeResAmt },
+    ].map(({ res, cost }) => {
+        const gameRes = game.resPool.get(res);
+        return {
+            res,
+            amt: Math.floor(gameRes.value / cost),
+        };
+    });
+
     const tradesPossible = Math.min(...tradeCaps.map(({amt}) => amt));
     const cappedResources = tradeCaps
         .filter(cap => cap.amt === tradesPossible)
         .map(cap => cap.res);
 
+    return {
+        tradesPossible,
+        cappedResources,
+    };
+}
+
+const tradeToCapTitanium = () => {
+    const { tradesPossible, cappedResources } = getCappedResourcesForTrading("slab", 50);
+
     if (tradesPossible === 0) {
-        this.game.msg(`Not enough ${cappedResources.join(",")} to trade with zebras`);
+        game.msg(`Not enough ${cappedResources.join(",")} to trade with zebras`);
         return;
     }
 
     const optimalZebraTrades = getOptimalZebraTrades();
     const nbTrades = Math.min(tradesPossible, optimalZebraTrades);
 
-    this.game.msg(`Approximately ${optimalZebraTrades} trades needed to cap Titanium`);
+    game.msg(`Approximately ${optimalZebraTrades} trades needed to cap Titanium`);
     if (nbTrades < optimalZebraTrades) {
-        this.game.msg(`Trade with zebras limited to ${nbTrades} by ${cappedResources.join(",")}`);
+        game.msg(`Trade with zebras limited to ${nbTrades} by ${cappedResources.join(",")}`);
     }
 
     var zebras = game.diplomacy.get("zebras");
     game.diplomacy.tradeMultiple(zebras, nbTrades);
+}
+
+
+/* Coal stuff */ 
+
+const coalTrades = [{ amt: 350, trades: 1 }];
+const average = arr => {
+    const { amt, trades } = arr.reduce( ( tally, current ) => ({
+        amt: tally.amt + current.amt,
+        trades: tally.trades + current.trades,
+    }), { amt: 0, trades: 0 });
+    return amt / trades;
+}
+
+const tradeWithSpiders = () => {
+    const { tradesPossible, cappedResources } = getCappedResourcesForTrading("scaffold", 50);
+
+    // If we don't have enough resources to trade
+    if (tradesPossible === 0) {
+        game.msg(`Not enough ${cappedResources.join(",")} to trade with spiders`);
+        return;
+    }
+
+    const coal = game.resPool.get("coal");
+    const iron = game.resPool.get("iron");
+    const coalNeededToCap = coal.maxValue - coal.value;
+    const desiredCoal = Math.min(iron.value - coal.value, coalNeededToCap);
+
+    // If we don't need any coal based on current coal and iron
+    if (desiredCoal <= 0) {
+        game.msg("No trades for coal needed");
+        return;
+    }
+
+    // Use a rolling average to determine approximate coal per trade
+    if (coalTrades.length > 10) {
+        coalTrades.shift();
+    }
+    const coalPerTrade = average(coalTrades);
+
+    if (coalPerTrade === 0) {
+        game.msg("Didn't get any coal... Not enough information to trade");
+        return;
+    }
+
+    const optimalSpiderTrades = Math.floor(desiredCoal / coalPerTrade) + 1;
+    const nbTrades = Math.min(optimalSpiderTrades, tradesPossible);
+
+    game.msg(`Approximately ${optimalSpiderTrades} trades needed to cap Coal`);
+    if (nbTrades < optimalSpiderTrades) {
+        game.msg(`Trade with spiders limited to ${nbTrades} by ${cappedResources.join(",")}`);
+    }
+
+    const oldCoal = game.resPool.get("coal").value;
+    var spiders = game.diplomacy.get("spiders");
+    game.diplomacy.tradeMultiple(spiders, nbTrades);
+
+    const newCoal = game.resPool.get("coal").value;
+    coalTrades.push({ trades: nbTrades, amt: newCoal - oldCoal });
 }
 
 /* Game Logic*/
@@ -87,10 +159,11 @@ const craftMyResources = (...extras) => {
 
 const header = document.getElementsByClassName("right-tab-header")[0];
 
-const addButton = (text, handleClick) => {
+const addButton = (text, handleClick, id) => {
     const newBtn = document.createElement("button");
     newBtn.appendChild(document.createTextNode(text));
     newBtn.onclick = handleClick;
+    newBtn.id = id;
     header.appendChild(newBtn);
 }
 
@@ -101,3 +174,4 @@ addButton("Make all the stuff", craftMyResources("steel", "plate"));
 addButton("Spend culture", spendCulture);
 addButton("Spend culture and praise", spendAndPray);
 addButton("Trade to cap titanium", tradeToCapTitanium);
+addButton("Trade for coal (match iron)", tradeWithSpiders, "spiderBtn");
